b3bb70375032b251ddbfedb2cbdb7cf484c558d4
SiwonHwang <kimshluv@naver.com>
2018-11-29 16:31:01 +0900

    02

--- keyword.c
+++ keyword.c
@@ -3,6 +3,10 @@
 #include <string.h>
 
 #define NKEYS       13
+struct keytab {
+	int num;
+	char keywrd[10];
+};  
 
 
 struct keytab keywrds[NKEYS] = {
@@ -19,13 +23,29 @@ struct keytab keywrds[NKEYS] = {
        {0, "default"},
        {0, "char"},
        {0, "do"}
-};
+};  
 
-void count_word(char* word){
+void count_word(char* word){ 
+	int i;
 	
+	for (i=0; i<NKEYS; i++)
+	{
+		if (strncmp(word, keywrds[i].keywrd, strlen(keywrds[i].keywrd)) == 0)
+		{
+			keywrds[i].num++;
+			break;
+		}
+		
+	}
 }
 
 
 void print_word(void) {
+	int i;
+	
+	for (i=0; i<NKEYS; i++)
+	{
+		printf("%s : %i\n", keywrds[i].keywrd, keywrds[i].num);
+	}
 	
 }
--- main.c
+++ main.c
@@ -3,9 +3,59 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+int is_whitespace(char c)
+{
+	if (c == ' ' ||
+	    c == '\t' ||
+		c == '\n' ||
+		c == '\r' ||
+		c == '(' ||
+		c == ')')
+		return 1; //공백글자다. 
+		
+	return 0; //유의미한 글자다. 
+ } 
+
+int fget_word(FILE* fp, char* word){
+	
+	char c;
+	int cnt;
+	
+	//유의미한 글자가 나올때까지 입력된 글자 버림 반복  
+	while ((c=fgetc(fp)) != EOF){
+		if (is_whitespace(c) == 0)
+		{
+			break; //무의미한 문자가 아니면 멈춤  
+		}
+	}
+	
+	if (c == EOF) //최초 읽은 문자가 마지막일때 (파일 끝일때) 
+	{
+		return 0;
+	}
+	
+	//본격적으로 단어를 한글자씩 읽음  
+	cnt=0;
+	word[cnt++] = c; //아까 읽은 첫글자 입력  
+	word[cnt] = '\0'; //혹시 뒤에서 못읽을 수 있으므로 (한글자짜리 단어) 
+	
+	while ((word[cnt]=fgetc(fp)) != EOF) //한 문자 받음  
+	{
+		if (is_whitespace(word[cnt]) == 1) //공백문자면 받은거 취소하고 나감  
+		{
+			word[cnt] = '\0';
+			break;
+		}
+		cnt++;
+	}
+	
+	return cnt;
+}
+
 int main(int argc, char *argv[]) {
 	FILE* fp;
 	char name[100];
+	char word[100];
 	
 	printf("Input a file name : ");
 	scanf("%s", name);
@@ -18,7 +68,11 @@ int main(int argc, char *argv[]) {
 		return -1;
 	}
 	
-	printf("%c\n", fgetc(fp));
+	while (fget_word(fp, word) != 0)
+	{
+		count_word(word); 
+	}
+	print_word();
 	
 	fclose(fp);
 		
